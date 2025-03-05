77aec211d384f942059ae316d42106c922de2215
Fróði Karlsson <frodikarlsson@gmail.com>
2025-03-05 15:46:30 +0100

    feat: add toggle and 10 char timestamp support

--- content.js
+++ content.js
@@ -1,16 +1,35 @@
-const numberLength13Regex = /(\d{13})/g;
+// We have to declare as var to avoid "already defined" error in user console
+var readableTimestampsAttrKey = "data-readable-ts-original-value";
+var alreadyModified = [
+  ...document.querySelectorAll(`[${readableTimestampsAttrKey}]`),
+];
 
 function replaceTextNodes(node) {
+  const numberLength11Or13Regex = /\b\d{10,13}\b/g;
   node.childNodes.forEach(function (el) {
     // If this is not a text node, or it is an empty text node, ignore it
     if (el.nodeType !== 3 || el.nodeValue.trim() === "") {
       replaceTextNodes(el);
       return;
     }
-    el.nodeValue = el.nodeValue.replace(numberLength13Regex, function (match) {
-      return new Date(parseInt(match)).toUTCString();
-    });
+    el.nodeValue = el.nodeValue.replace(
+      numberLength11Or13Regex,
+      function (match) {
+        el.parentElement?.setAttribute(readableTimestampsAttrKey, el.nodeValue);
+        return new Date(
+          parseInt(match.length === 10 ? match + "000" : match)
+        ).toLocaleString();
+      }
+    );
   });
 }
 
-replaceTextNodes(document.body);
+// Allows the extension to toggle between the original and modified values
+if (alreadyModified.length) {
+  alreadyModified.forEach(function (el) {
+    el.innerText = el.getAttribute(readableTimestampsAttrKey);
+    el.removeAttribute(readableTimestampsAttrKey);
+  });
+} else {
+  replaceTextNodes(document.body);
+}
